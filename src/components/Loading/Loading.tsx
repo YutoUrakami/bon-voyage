4fc0c120e7825b87f19e794f235a10074caf281a
YutoUrakami <urakami@loilo.tv>
2019-02-09 23:28:42 +0900

    feat: loading animation

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
 import './Loading.css'
+import {CSSTransition} from "react-transition-group";
 
 class Loading extends React.Component {
   public render() {
     return (
-      <div className="Loading">now loading...</div>
+      <CSSTransition
+        classNames="loading"
+        in={true}
+        appear={true}
+        mountOnEnter={true}
+        timeout={750}>
+        <div className="Loading">
+          <div className="lds-dual-ring"/>
+        </div>
+      </CSSTransition>
     )
   }
 }
