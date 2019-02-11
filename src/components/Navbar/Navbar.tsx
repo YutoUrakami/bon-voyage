ca4d087268c7910abadd5ef2e980aa7eaa27484e
YutoUrakami <urakami@loilo.tv>
2019-02-11 21:41:32 +0900

    feat: burger menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import * as cloudFunctions from '../../services/cloudFunctions'
 import './Navbar.css';
 
 interface NavbarState {
+  burgerActive: boolean,
   folders: string[]
 }
 
@@ -11,8 +12,11 @@ class Navbar extends React.Component<{}, NavbarState> {
   constructor(props: any) {
     super(props);
     this.state = {
+      burgerActive: false,
       folders: []
-    }
+    };
+    this.toggleBurgerActive = this.toggleBurgerActive.bind(this);
+    this.clearBurgerActive = this.clearBurgerActive.bind(this);
   }
 
   public render() {
@@ -23,17 +27,17 @@ class Navbar extends React.Component<{}, NavbarState> {
             <p className="main-brand">bon voyage!</p>
           </a>
           <p className="navbar-item sub-brand">phoooutty's photo gallery</p>
-          <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false"
-             data-target="navbarBasicExample">
+          <a role="button" className={this.burgerClassName()} aria-label="menu" aria-expanded="false"
+             data-target="navbarBasicExample" onClick={this.toggleBurgerActive}>
             <span aria-hidden="true"/>
             <span aria-hidden="true"/>
             <span aria-hidden="true"/>
           </a>
         </div>
-        <div className="navbar-menu">
+        <div className={this.menuClassName()}>
           <div className="navbar-end">
             {this.state.folders.map((folder) => {
-              return <Link to={`/${folder}`} className="navbar-item" key={folder}>{folder}</Link>
+              return <Link to={`/${folder}`} className="navbar-item" key={folder} onClick={this.clearBurgerActive}>{folder}</Link>
             })}
           </div>
         </div>
@@ -45,6 +49,23 @@ class Navbar extends React.Component<{}, NavbarState> {
     cloudFunctions.listFolders().then((folderNames: string[]) => {
       this.setState({folders: folderNames})
     });
+  }
+  
+  private burgerClassName() {
+    return `navbar-burger burger ${this.state.burgerActive ? "is-active" : ""}`
+  }
+  
+  private menuClassName() {
+    return `navbar-menu ${this.state.burgerActive ? "is-active" : ""}`
+  }
+  
+  private toggleBurgerActive() {
+    const next = !this.state.burgerActive;
+    this.setState({burgerActive: next});
+  }
+  
+  private clearBurgerActive() {
+    this.setState({burgerActive: false});
   }
 }
 
