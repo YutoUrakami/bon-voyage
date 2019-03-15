2594d9fc75410ed9700a8e7c869ab1f534a8e298
YutoUrakami <urakami@loilo.tv>
2019-03-15 23:09:40 +0900

    refact: functionsのregionを東京にして、rewritesによるhostingとの接続をやめた #11

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,21 +7,21 @@ const cors = CORS({
   origin: "https://photo.phoooutty.com"
 });
 
-export const listFolders = functions.https.onRequest( (req, res) => {
+export const listFolders = functions.region("asia-northeast1").https.onRequest( (req, res) => {
   cors(req, res, async () => {
     const cloudinaryRes = await listFolderHandler();
     res.status(cloudinaryRes.status).send(cloudinaryRes.data);
   });
 });
 
-export const listImagesByTag = functions.https.onRequest( (req, res) => {
+export const listImagesByTag = functions.region("asia-northeast1").https.onRequest( (req, res) => {
   cors(req, res, async () => {
     const cloudinaryRes = await listImagesByTagHandler(req.query.tag);
     res.status(cloudinaryRes.status).send(cloudinaryRes.data);
   });
 });
 
-export const listImagesInFolder = functions.https.onRequest( (req, res) => {
+export const listImagesInFolder = functions.region("asia-northeast1").https.onRequest( (req, res) => {
   cors(req, res, async () => {
     const cloudinaryRes = await listImagesInFolderHandler(req.query.folder_name);
     res.status(cloudinaryRes.status).send(cloudinaryRes.data);
