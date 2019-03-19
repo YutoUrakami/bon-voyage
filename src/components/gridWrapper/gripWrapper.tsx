4b11a4974302355d8239246c9c4140f003abe580
YutoUrakami <urakami@loilo.tv>
2019-03-20 00:37:59 +0900

    feat: ページ毎のtitle設定 #24

--- src/components/gridWrapper/gripWrapper.tsx
+++ src/components/gridWrapper/gripWrapper.tsx
@@ -3,6 +3,7 @@ import {connect} from "react-redux";
 import {Dispatch} from "redux";
 import {listingInFolder} from "../../reducers/imagesListReducer";
 import Grid from '../grid/grid'
+import * as title from "../../helpers/title"
 
 interface GripWrapperProps {
   folderName: string
@@ -18,11 +19,13 @@ class GripWrapper extends React.Component<GripWrapperProps & DispatchProps> {
   }
 
   public componentWillMount(): void {
-    listingInFolder(this.props.folderName)(this.props.dispatch)
+    listingInFolder(this.props.folderName)(this.props.dispatch);
+    title.set(this.props.folderName);
   }
   
   public componentWillReceiveProps(nextProps: GripWrapperProps & DispatchProps) {
-    listingInFolder(nextProps.folderName)(nextProps.dispatch)
+    listingInFolder(nextProps.folderName)(nextProps.dispatch);
+    title.set(nextProps.folderName);
   }
 }
 export default connect()(GripWrapper);
