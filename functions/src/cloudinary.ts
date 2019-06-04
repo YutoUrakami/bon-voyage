b7c2f7094476b6254d4e4c255827bdf632880cfb
YutoUrakami <urakami@loilo.tv>
2019-06-05 01:33:58 +0900

    fix: フォルダ内の画像が10件しか取れていない

--- functions/src/cloudinary.ts
+++ functions/src/cloudinary.ts
@@ -1,8 +1,8 @@
 import * as env from './env'
 import cloudinary = require('cloudinary');
 
-const cldny = cloudinary.v2;
-cldny.config({
+const cloudinaryV2 = cloudinary.v2;
+cloudinaryV2.config({
   cloud_name: env.CLOUDINARY_CLOUDNAME,
   api_key: env.CLOUDINARY_API_KEY,
   api_secret: env.CLOUDINARY_API_SECRET
@@ -11,7 +11,7 @@ cldny.config({
 export const listFolderHandler = async () => {
   return new Promise((resolve, reject) => {
     console.log("will get folders");
-    cldny.api.root_folders((error, result) => {
+    cloudinaryV2.api.root_folders((error, result) => {
       if (error) {
         reject(error);
       }
@@ -24,7 +24,7 @@ export const listFolderHandler = async () => {
 export const listImagesByTagHandler = async (tag: string) => {
   return new Promise((resolve, reject) => {
     console.log(`will get resources with tag ${tag}`);
-    cldny.api.resources({ type: 'upload', tags: tag }, (error, result) => {
+    cloudinaryV2.api.resources({type: 'upload', tags: tag}, (error, result) => {
       if (error) {
         reject(error);
       }
@@ -36,11 +36,17 @@ export const listImagesByTagHandler = async (tag: string) => {
 export const listImagesInFolderHandler = async (folderName: string) => {
   console.log(`will get resources with prefix ${folderName}/`);
   return new Promise((resolve, reject) => {
-    cldny.api.resources({ type: 'upload', prefix: `${folderName}/` }, (error, result) => {
-      if (error) {
-        reject(error);
-      }
-      resolve(result);
-    })
+    cloudinaryV2.api.resources(
+      {
+        type: 'upload',
+        prefix: `${folderName}/`,
+        max_results: 500,
+        context: true
+      }, (error, result) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(result);
+      })
   });
 };
